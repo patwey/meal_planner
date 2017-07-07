37d01c3c87505a08da6ba925cfc6592094862d1c
Pat Wey <patrickwey@gmail.com>
2017-07-06 20:44:31 -0700

    Add sign in redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   end
 
   def user_not_authorized
-    flash[:alert] = "Oops, you're not authorized to do that"
-    redirect_to(request.referrer || root_path)
+    redirect_back fallback_location: root_path,
+                  alert: "Oops, you're not authorized to do that"
   end
 end
